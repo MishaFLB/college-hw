003f0bf054d61c4bd5aa1af5ba010fbd1e18c045
MishaFLB <136157481+MishaFLB@users.noreply.github.com>
2025-02-13 21:13:50 +0200

    Add files via upload

---
+++ ПростаЗадачка3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    std::cout << "Введіть число a:";
+    std::cin >> a;
+    std::cout << "Введіть число b:";
+    std::cin >> b;
+
+    std::cout << "Сума:" << (a + b) << std::endl;
+    std::cout << "Різниця:" << (a - b) << std::endl;
+    std::cout << "Добуток:" << (a * b) << std::endl;
+    return 0;
+}
